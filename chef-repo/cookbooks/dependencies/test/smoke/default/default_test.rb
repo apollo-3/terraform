fc11c0668c3219d7af8f8f4302daafb58c0b8f67
Apollo <apollo-3@mail.ru>
2018-02-13 12:09:50 +0000

    added kitchen tests for dependencies cookbook

--- chef-repo/cookbooks/dependencies/test/smoke/default/default_test.rb
+++ chef-repo/cookbooks/dependencies/test/smoke/default/default_test.rb
@@ -5,14 +5,31 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
+packages   = ['mysql-community-devel',
+              'autoconf',
+              'automake',
+              'gcc',
+              'gcc-c++',
+              'libtool',
+              'make']
+
+gems       = ['mysql2', 'aws-sdk']
+
+mysql_repo = "mysql-community"
+
+describe yum.repo(mysql_repo) do
+  it { should exist }
+  it { should be_enabled }
+end
+
+packages.each do |pkg|
+  describe package(pkg) do
+    it { should be_installed }
   end
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+gems.each do |gem_item|
+  describe gem(gem_item, :chef) do
+    it { should be_installed }
+  end
 end
